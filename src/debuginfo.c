763fe25506634cb8c37e8e4c9f4b3e159748e8a7
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2020-04-30 00:05:16 +0000

    Add sizeof and offsetof primitives for modules

--- src/debuginfo.c
+++ src/debuginfo.c
@@ -5,12 +5,22 @@
 
 #include "ipftrace.h"
 
-void debuginfo_destroy(struct ipft_debuginfo *dinfo) {
-  dinfo->destroy(dinfo);
-  free(dinfo);
-}
-
 int debuginfo_fill_sym2info(struct ipft_debuginfo *dinfo,
                             struct ipft_symsdb *sdb) {
   return dinfo->fill_sym2info(dinfo, sdb);
+}
+
+int debuginfo_sizeof(struct ipft_debuginfo *dinfo,
+    const char *type, size_t *sizep) {
+  return dinfo->sizeof_fn(dinfo, type, sizep);
+}
+
+int debuginfo_offsetof(struct ipft_debuginfo *dinfo,
+    const char *type, const char *member, size_t *offsetp) {
+  return dinfo->offsetof_fn(dinfo, type, member, offsetp);
+}
+
+void debuginfo_destroy(struct ipft_debuginfo *dinfo) {
+  dinfo->destroy(dinfo);
+  free(dinfo);
 }
