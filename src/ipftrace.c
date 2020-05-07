9356a44f242546928e0ae8cab48c144142f6b832
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2020-05-07 17:48:57 +0000

    Update CLI interface for latest source

--- src/ipftrace.c
+++ src/ipftrace.c
@@ -11,12 +11,10 @@
 #include "ipftrace.h"
 
 static struct option options[] = {
-    {"verbose", required_argument, 0, 'v'},
-    {"mark", required_argument, 0, 'm'},
     {"debug-format", required_argument, 0, 'f'},
-    {"mark-offset", required_argument, 0, 0},
-    {"vmlinux-path", required_argument, 0, 0},
-    {"modules-path", required_argument, 0, 0},
+    {"mark", required_argument, 0, 'm'},
+    {"script", optional_argument, 0, 's'},
+    {"perf-page-count", optional_argument, 0, '0'},
     {NULL, 0, 0, 0},
 };
 
@@ -24,51 +22,40 @@ static void usage(void) {
   fprintf(stderr, "Usage: ipftrace [OPTIONS]\n"
                   "\n"
                   "Options:\n"
-                  " -v, --verbose       [LEVEL]           Set log level\n"
                   " -m, --mark          [MARK]            Trace the packet "
                   "marked with <mark> [required]\n"
                   " -f, --debug-format  [DEBUG-FORMAT]    Read the debug "
                   "information with specified format\n"
-                  "   , --mark-offset   [OFFSET]          Specify the offset "
-                  "of the `mark` field of sk_buff manually\n"
-                  "   , --vmlinux-path  [PATH]            Specify the vmlinux "
-                  "path manually\n"
-                  "   , --modules-path  [PATH]            Specify the modules "
-                  "path manually\n"
+                  " -s, --script-path   [PATH]            Path to the Lua script file"
                   "\n"
-                  "LEVEL        := { 0, 1, 2, 3, 4 }\n"
                   "MARK         := hex number\n"
                   "DEBUG-FORMAT := { dwarf, btf }\n"
-                  "OFFSET       := integer\n"
                   "PATH         := path\n"
                   "\n");
   exit(EXIT_FAILURE);
 }
 
-static void opt_init(struct ipft_opt *opt) {
-  opt->verbose = 0;
+static void opt_init(struct ipft_tracer_opt *opt) {
   opt->mark = 0;
-  opt->debug_format = "dwarf";
-  opt->mark_offset = 0;
-  opt->vmlinux_path = NULL;
-  opt->modules_path = NULL;
+  opt->script_path = NULL;
+  opt->debug_info_type = "dwarf";
+  opt->perf_page_cnt = 8;
 }
 
-static bool opt_validate(struct ipft_opt *opt) {
-  if (opt->verbose < 0 || opt->verbose >= IPFT_LOG_MAX) {
-    fprintf(stderr, "Invalid verbose level. It should be 0 < level < %d\n",
-            IPFT_LOG_MAX);
-    return false;
-  }
-
+static bool opt_validate(struct ipft_tracer_opt *opt) {
   if (opt->mark == 0) {
-    fprintf(stderr, "-m --mark is missing\n");
+    fprintf(stderr, "-m --mark is missing (or specified 0 which is invalid)\n");
     return false;
   }
 
-  if (strcmp(opt->debug_format, "dwarf") != 0 &&
-      strcmp(opt->debug_format, "btf") != 0) {
-    fprintf(stderr, "Invalid debug format %s\n", opt->debug_format);
+  if (strcmp(opt->debug_info_type, "dwarf") != 0 &&
+      strcmp(opt->debug_info_type, "btf") != 0) {
+    fprintf(stderr, "Invalid debug info format %s\n", opt->debug_info_type);
+    return false;
+  }
+
+  if (opt->perf_page_cnt == 0) {
+    fprintf(stderr, "Perf page count should be at least 1\n");
     return false;
   }
 
@@ -76,43 +63,33 @@ static bool opt_validate(struct ipft_opt *opt) {
 }
 
 int main(int argc, char **argv) {
-  int c, optind;
+  int error, c, optind;
   const char *optname;
-  struct ipft_opt opt;
+  struct ipft_tracer_opt opt;
 
   opt_init(&opt);
 
-  while ((c = getopt_long(argc, argv, "v:m:f:0", options, &optind)) != -1) {
+  while ((c = getopt_long(argc, argv, "f:m:s:0", options, &optind)) != -1) {
     switch (c) {
-    case 'v':
-      opt.verbose = atoi(optarg);
+    case 'f':
+      opt.debug_info_type = strdup(optarg);
       break;
     case 'm':
       opt.mark = strtoul(optarg, NULL, 16);
       break;
-    case 'f':
-      opt.debug_format = strdup(optarg);
+    case 's':
+      opt.script_path = strdup(optarg);
       break;
-    case 0:
+    case '0':
       optname = options[optind].name;
 
-      if (strcmp(optname, "mark-offset") == 0) {
-        opt.mark_offset = strtoul(optarg, NULL, 10);
-        break;
-      }
-
       if (strcmp(optname, "debug-format") == 0) {
-        opt.debug_format = strdup(optarg);
+        opt.debug_info_type = strdup(optarg);
         break;
       }
 
-      if (strcmp(optname, "vmlinux-path") == 0) {
-        opt.vmlinux_path = strdup(optarg);
-        break;
-      }
-
-      if (strcmp(optname, "modules-path") == 0) {
-        opt.modules_path = strdup(optarg);
+      if (strcmp(optname, "perf-page-count") == 0) {
+        opt.perf_page_cnt = atoi(optarg);
         break;
       }
 
@@ -127,7 +104,11 @@ int main(int argc, char **argv) {
     usage();
   }
 
-  do_trace(&opt);
+  error = tracer_run(&opt);
+  if (error == -1) {
+    fprintf(stderr, "Trace failed with error\n");
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
