edf532036cb8755819166a15cae8693be761e108
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2020-05-09 14:02:49 +0000

    Stringify the gso flags

--- scripts/gso.lua
+++ scripts/gso.lua
@@ -1,13 +1,40 @@
-uint_size       = ipft_sizeof("unsigned int")
-ushort_size     = ipft_sizeof("short unsigned int")
-ptr_size        = ipft_sizeof("uintptr_t")
-skb_data_size   = ipft_sizeof("sk_buff_data_t")
-len_offset      = ipft_offsetof("sk_buff", "len")
-head_offset     = ipft_offsetof("sk_buff", "head")
-end_offset      = ipft_offsetof("sk_buff", "end")
-gso_size_offset = ipft_offsetof("skb_shared_info", "gso_size")
-gso_segs_offset = ipft_offsetof("skb_shared_info", "gso_segs")
-gso_type_offset = ipft_offsetof("skb_shared_info", "gso_type")
+GSO_FLAGS = {
+  [ 1 << 0 ]  = "tcpv4",
+  [ 1 << 1 ]  = "dodgy",
+  [ 1 << 2 ]  = "tcp-ecn",
+  [ 1 << 3 ]  = "tcp-fixedid",
+  [ 1 << 4 ]  = "tcpv6",
+  [ 1 << 5 ]  = "fcoe",
+  [ 1 << 6 ]  = "gre",
+  [ 1 << 7 ]  = "gre-csum",
+  [ 1 << 8 ]  = "ipxip4",
+  [ 1 << 9 ]  = "ipxip6",
+  [ 1 << 10 ] = "udp-tunnel",
+  [ 1 << 11 ] = "udp-tunnel-csum",
+  [ 1 << 12 ] = "partial",
+  [ 1 << 13 ] = "tunnel-remcsum",
+  [ 1 << 14 ] = "sctp",
+  [ 1 << 15 ] = "esp",
+  [ 1 << 16 ] = "udp",
+  [ 1 << 17 ] = "udp-l4",
+  [ 1 << 18 ] = "flaglist",
+}
+
+function flags2str(flags)
+  ret = ""
+  is_first = true
+  for k, v in pairs(GSO_FLAGS) do
+    if flags & k ~=  0 then
+      if not is_first then
+        ret = ret.."|"..v
+      else
+        ret = ret..v
+        is_first = false
+      end
+    end
+  end
+  return ret
+end
 
 function size2code(size)
   if size == 1 then
@@ -24,6 +51,17 @@ function size2code(size)
 end
 
 function emit()
+  uint_size       = ipft_sizeof("unsigned int")
+  ushort_size     = ipft_sizeof("short unsigned int")
+  ptr_size        = ipft_sizeof("uintptr_t")
+  skb_data_size   = ipft_sizeof("sk_buff_data_t")
+  len_offset      = ipft_offsetof("sk_buff", "len")
+  head_offset     = ipft_offsetof("sk_buff", "head")
+  end_offset      = ipft_offsetof("sk_buff", "end")
+  gso_size_offset = ipft_offsetof("skb_shared_info", "gso_size")
+  gso_segs_offset = ipft_offsetof("skb_shared_info", "gso_segs")
+  gso_type_offset = ipft_offsetof("skb_shared_info", "gso_type")
+
   if skb_data_size == uint_size then
     return BPF.emit({
       -- Save callee saved registers
@@ -93,5 +131,6 @@ end
 function dump(data)
   format = string.format("=I%dI%dI%dI%d", uint_size, ushort_size, ushort_size, uint_size)
   len, gso_size, gso_segs, gso_type = string.unpack(format, data)
-  return string.format("(len: %d size: %d segs: %d type: 0x%x)", len, gso_size, gso_segs, gso_type)
+  return string.format("(len: %d gso_size: %d gso_segs: %d gso_type: %s)",
+                       len, gso_size, gso_segs, flags2str(gso_type))
 end
