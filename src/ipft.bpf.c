4a3edba0aa58710092c8bfba4278185654f95f65
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2020-12-13 16:38:00 +0000

    Don't use BPF_KPROBE, this makes code shorter

--- src/ipft.bpf.c
+++ src/ipft.bpf.c
@@ -55,28 +55,37 @@ ipft_body(struct pt_regs *ctx, struct sk_buff *skb)
 }
 
 SEC("kprobe/ipft_main1") void
-BPF_KPROBE(ipft_main1, struct sk_buff *skb)
+ipft_main1(struct pt_regs *ctx)
 {
+  struct sk_buff *skb = (struct sk_buff *)PT_REGS_PARM1(ctx);
   ipft_body(ctx, skb);
 }
 
 SEC("kprobe/ipft_main2") void
-BPF_KPROBE(ipft_main2, uint64_t arg1, struct sk_buff *skb) {
+ipft_main2(struct pt_regs *ctx)
+{
+  struct sk_buff *skb = (struct sk_buff *)PT_REGS_PARM2(ctx);
   ipft_body(ctx, skb);
 }
 
 SEC("kprobe/ipft_main3") void
-BPF_KPROBE(ipft_main3, uint64_t arg1, uint64_t arg2, struct sk_buff *skb) {
+ipft_main3(struct pt_regs *ctx)
+{
+  struct sk_buff *skb = (struct sk_buff *)PT_REGS_PARM3(ctx);
   ipft_body(ctx, skb);
 }
 
 SEC("kprobe/ipft_main4") void
-BPF_KPROBE(ipft_main4, uint64_t arg1, uint64_t arg2, uint64_t arg3, struct sk_buff *skb) {
+ipft_main4(struct pt_regs *ctx)
+{
+  struct sk_buff *skb = (struct sk_buff *)PT_REGS_PARM4(ctx);
   ipft_body(ctx, skb);
 }
 
 SEC("kprobe/ipft_main5") void
-BPF_KPROBE(ipft_main5, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, struct sk_buff *skb) {
+ipft_main5(struct pt_regs *ctx)
+{
+  struct sk_buff *skb = (struct sk_buff *)PT_REGS_PARM5(ctx);
   ipft_body(ctx, skb);
 }
 
