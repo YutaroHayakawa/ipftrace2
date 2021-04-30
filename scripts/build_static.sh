0a703cda1bca3bda2089bac09e57c3edd93d2faf
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2021-04-30 09:53:35 +0000

    Split out default module as null_module and link it

--- scripts/build_static.sh
+++ scripts/build_static.sh
@@ -5,4 +5,8 @@
 docker build -t ipftrace:latest -f docker/Dockerfile.build_static .
 docker run --name ipft --rm -d --entrypoint /sbin/init ipftrace:latest
 docker cp ipft:/ipftrace2/src/ipft .
+docker cp ipft:/ipftrace2/src/ipft.bpf.o src/ipft.bpf.o
+docker cp ipft:/ipftrace2/src/ipft.bpf.o.h src/ipft.bpf.o.h
+docker cp ipft:/ipftrace2/src/null_module.bpf.o src/null_module.bpf.o
+docker cp ipft:/ipftrace2/src/null_module.bpf.o.h src/null_module.bpf.o.h
 docker stop ipft
