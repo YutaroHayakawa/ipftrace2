8e1319ffda822c7845d3194efbfc6d8698a667d6
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2021-05-01 17:33:25 +0000

    Don't need dedicated directory for Dockerfile

--- scripts/build_static.sh
+++ scripts/build_static.sh
@@ -2,7 +2,7 @@
 
 # Run this in project top
 
-docker build -t ipftrace:latest -f docker/Dockerfile.build_static .
+docker build -t ipftrace:latest .
 docker run --name ipft --rm -d --entrypoint /sbin/init ipftrace:latest
 docker cp ipft:/ipftrace2/src/ipft .
 docker cp ipft:/ipftrace2/src/ipft.bpf.o src/ipft.bpf.o
