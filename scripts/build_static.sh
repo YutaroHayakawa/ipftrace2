b7a5d37c8efce3798249bfcb3c09b62113780a41
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2021-12-07 23:11:33 +0900

    Fix wrong image name

--- scripts/build_static.sh
+++ scripts/build_static.sh
@@ -6,11 +6,11 @@ ROOT=$(pwd)
 VERSION=$(git describe --tags --abbrev=0)
 
 if [ -z "$DOCKER_IMAGE" ]; then
-    DOCKER_IMAGE="yutarohayakawa/ipftrace2:$VERSION"
+    DOCKER_IMAGE="yutarohayakawa/ipftrace2-builder:$VERSION"
 fi
 
 if [ -z "$CONTAINER_NAME" ]; then
-    CONTAINER_NAME="ipftrace2"
+    CONTAINER_NAME="ipftrace2-builder"
 fi
 
 docker run -it --rm --name $CONTAINER_NAME -v $ROOT:/mnt $DOCKER_IMAGE /bin/sh -c "cd /mnt/src && make"
