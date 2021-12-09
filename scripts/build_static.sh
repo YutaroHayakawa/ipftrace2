15eb1b47640cc86460ce2bee7a4cc12deb7ca06a
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2021-12-09 10:55:35 +0000

    Make scripts.build_static.sh to specify target

--- scripts/build_static.sh
+++ scripts/build_static.sh
@@ -2,6 +2,7 @@
 
 # Run this in project top
 
+TARGET=""
 ROOT=$(pwd)
 VERSION=$(git describe --tags --abbrev=0)
 
@@ -13,4 +14,8 @@ if [ -z "$CONTAINER_NAME" ]; then
     CONTAINER_NAME="ipftrace2-builder"
 fi
 
-docker run --rm --name $CONTAINER_NAME -v $ROOT:/mnt $DOCKER_IMAGE /bin/sh -c "cd /mnt/src && make"
+if [ ! -z "$1" ]; then
+    TARGET=$1
+fi
+
+docker run --rm --name $CONTAINER_NAME -v $ROOT:/mnt $DOCKER_IMAGE /bin/sh -c "cd /mnt/src && make $TARGET"
