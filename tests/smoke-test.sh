4bd7dcfbbd8b30b134b9b989d509bab479ea0df0
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2024-04-06 16:46:37 +0900

    Use Little VM Helper for testing
    Signed-off-by: Yutaro Hayakawa <yhayakawa3720@gmail.com>

--- tests/smoke-test.sh
+++ tests/smoke-test.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+if [ -z $IPFT ]; then
+  IPFT=ipft
+fi
+
 for tracer in "$@"; do
   # Launch function tracer
-  ipft -v -m 1 -t $tracer -o json --enable-probe-server > /tmp/trace.txt &
+  $IPFT -v -m 1 -t $tracer -o json --enable-probe-server > /tmp/trace.txt &
 
   # Wait for tracer to be ready
   until nc -z 127.0.0.1 13720; do sleep 2s; done
@@ -19,18 +23,3 @@ for tracer in "$@"; do
   # Wait for ipft to finish
   wait
 done
-
-# Run with BPF module
-ipft --gen bpf-module-skeleton > extension.c
-
-ipft -v -m 1 -o json --enable-probe-server -e extension.c > /tmp/trace.txt &
-
-until nc -z 127.0.0.1 13720; do sleep 2s; done
-
-ping -c 1 -m 1 127.0.0.1
-
-pkill -e -TERM ipft
-
-cat /tmp/trace.txt | jq -r
-
-wait
